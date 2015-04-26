9b34300153f57b2caa08b8ad67f671efcf1f3f15
Dana Fallon <danajfallon@gmail.com>
2015-04-25 21:03:43 -0700

    finished parts 1&2: HTML & CSS

--- application.py
+++ application.py
@@ -4,13 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    # Return this as a strange
-    return "<html><body>This is the homepage.</body></html>"
+	return render_template("index.html")
 
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
-    # return render_template("index.html")
+@app.route("/")
 
 if __name__ == "__main__":
     app.run(debug=True)

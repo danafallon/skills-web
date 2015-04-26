bbe0892abc342ccc8b3f447917616bf1d2f61c0d
Dana Fallon <danajfallon@gmail.com>
2015-04-25 21:42:56 -0700

    finished part 3: flask

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,20 @@ app = Flask(__name__)
 def index_page():
 	return render_template("index.html")
 
-@app.route("/")
+@app.route("/application-form")
+def application_page():
+	return render_template("application-form.html")
+
+@app.route("/application", methods=["POST"])
+def acknowledge_application():
+	job_titles = {"swe": "Software Engineer", "qae": "QA Engineer", "pm": "Product Manager"}
+	firstname = request.form.get("firstname")
+	lastname = request.form.get("lastname")
+	name = firstname + " " + lastname
+	job = request.form.get("job")
+	job_title = job_titles[job]
+	salary = request.form.get("salary")
+	return render_template("acknowledge.html", name=name, job=job_title, salary=salary)
 
 if __name__ == "__main__":
     app.run(debug=True)
